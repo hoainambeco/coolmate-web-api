ae98617ca6f51aed53f2ce9afaaa095481eb4aef
PhamVTTho <pvttho100502@gmail.com>
2022-09-29 20:49:33 +0700

    product

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('index')
+  @Render('listProduct')
   root() {
     return this.appService.getHello();
   }
