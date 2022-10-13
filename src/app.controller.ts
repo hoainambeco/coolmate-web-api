0d3f739cbf787d3f28326a440d7358d0b8b94ede
PhamVTTho <pvttho100502@gmail.com>
2022-10-13 09:09:39 +0700

    href

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,33 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('profile')
+  @Render('addProduct')
   root() {
+    return this.appService.getHello();
+  }
+  @Get('product')
+  @Render('listProduct')
+  getListProduct() {
+    return this.appService.getHello();
+  }
+  @Get('product-add')
+  @Render('addProduct')
+  addProduct() {
+    return this.appService.getHello();
+  }
+  @Get('profile')
+  @Render('profile')
+  getProfile() {
+    return this.appService.getHello();
+  }
+  @Get('users')
+  @Render('listUser')
+  getListUser() {
+    return this.appService.getHello();
+  }
+  @Get('login')
+  @Render('login')
+  getlogin() {
     return this.appService.getHello();
   }
 }
