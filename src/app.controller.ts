e9dcacc4943042981596802b3b4a42970760ef10
hoainambeco <namxg1@gmail.com>
2022-12-07 00:06:09 +0700

    fix rss

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,6 +20,7 @@ import { extname } from "path";
 import { JwtAuthGuard } from "./guards/jwt-auth.guard";
 import { AuthUserInterceptor } from "./interceptors/auth-user.interceptor";
 import { UserDto } from "./users/dto/user.dto";
+import fetch from "node-fetch";
 
 export const imageFileFilter = (req, file, callback) => {
   console.log(file);
